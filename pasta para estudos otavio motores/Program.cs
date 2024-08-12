e3526a133e3e59766490fb35c0c23b3d2e0b6751
otaviofreita <otavio.freitas@escolar.ifrn.edu.br>
2024-08-12 11:47:28 -0300

    atividades

---
+++ pasta para estudos otavio motores/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Console.WriteLine("Digite uma nota";)
+int nota = int.Parse(Console.ReadLine());
+
+if(nota >= 60)
+{ 
+Console.WriteLine(Aprovado);
+}
+else if (nota >= 20)
+{
+Console.WriteLine
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
